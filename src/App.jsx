b82b0dae397b167d70d463344681234453d7a7a7
gafs-codex <abdulmuizgafarcodes@gmail.com>
2026-05-02 14:38:22 +0100

    adding login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useParams, Navigate } from 'react-router-dom'
 import { useState } from 'react'
 import Home from './pages/Home'
 import About from './pages/About'
 import Dashboard from './pages/Dashboard'
 import Navbar from './components/Navbar'
 import UserProfile from './pages/UserProfile'
+import Login from './pages/Login'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
   function NotFound() {
     return <div className='center'>
       <h1 style={{ color: "red", fontSize: "50px" }}>404 - Page not found</h1>
@@ -16,13 +18,15 @@ function App() {
   }
   return (
     <BrowserRouter>
-      <Navbar /> {/* always visible on every page */}
+      {isLoggedIn && <Navbar />} {/* always visible on every page */}
 
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/users/:id' element={<UserProfile />} />
+        <Route path='/login' element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+
+        <Route path='/' element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
+        <Route path='/about' element={isLoggedIn ? <About /> : <Navigate to="/login" />} />
+        <Route path='/dashboard' element={isLoggedIn ? <Dashboard /> : <Navigate to="/login" />} />
+        <Route path='/users/:id' element={isLoggedIn ? <UserProfile /> : <Navigate to="/login" />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+function Login(props) {
+    const navigate = useNavigate()
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (email === "gafmuiz@gmail.com" && password === "secret") {
+            alert(`Welcome ${email}`)
+            props.setisLoggedIn(true)
+            navigate("/")
+        }
+        else {
+            alert("Wrong credentials")
+        }
+    }
+
+    return (
+        <div className="login-container">
+            <h2>Log in</h2>
+            <form action="" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email </label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </div>
+                <br />
+                <div>
+                    <label htmlFor="password">Password </label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                </div>
+                <br />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+export default Login;
