4c2cb67425d0040112b1a46154970d464e9d60a7
gafs-codex <abdulmuizgafarcodes@gmail.com>
2026-05-02 15:11:12 +0100

    rounding off logins

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import './login.css'
 function Login(props) {
     const navigate = useNavigate()
     const [email, setEmail] = useState("");
@@ -10,7 +11,7 @@ function Login(props) {
 
         if (email === "gafmuiz@gmail.com" && password === "secret") {
             alert(`Welcome ${email}`)
-            props.setisLoggedIn(true)
+            props.setIsLoggedIn(true)
             navigate("/")
         }
         else {
@@ -19,31 +20,33 @@ function Login(props) {
     }
 
     return (
-        <div className="login-container">
-            <h2>Log in</h2>
-            <form action="" onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">Email </label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                </div>
-                <br />
-                <div>
-                    <label htmlFor="password">Password </label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                </div>
-                <br />
-                <button type="submit">Login</button>
-            </form>
+        <div className="login-body">
+            <div className="login-container">
+                <h2>Log in</h2>
+                <form action="" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="email">Email </label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <br />
+                    <div>
+                        <label htmlFor="password">Password </label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <br />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
         </div>
     )
 }
